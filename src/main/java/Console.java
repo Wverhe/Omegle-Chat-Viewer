c03ad6958a1e76b3a859feda71ff3219e9a0684a
agaspari-h <wverhesaythe@gmail.com>
2017-06-10 21:46:06 -0500

    Change project from JavaFX -> Maven

--- src/main/java/Console.java
+++ src/main/java/Console.java
@@ -1,5 +1,3 @@
-package sample;
-
 /**
  * Created by jetma on 6/10/2017.
  */
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,3 @@
-package sample;
-
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
